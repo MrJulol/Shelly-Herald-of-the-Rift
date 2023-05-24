f24f94fc424d90ea617cda1ddcee283ff124fd80
MrJulol <julian.thaler06@gmail.com>
2023-05-24 10:36:04 +0200

    Update startmenu()

--- shell.c
+++ shell.c
@@ -38,7 +38,7 @@ void start_menu()
     printf("Shelly, Herald of the Rift\n");
     printf("Version 1.0\n");
     printf("By Juls, aLeg, iVan and Schle\n");
-    printf("Usable Commands: touch, ls (ADD COMMANDS HERE)\n");
+    printf("Usable Commands: touch, ls, rm, mkdir, sl (ADD COMMANDS HERE)\n");
 }
 
 void userbar()
